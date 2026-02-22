0684da55052ba2d8987f2feb51bc886cc272e1e6
Harsha Buddhika <59781888+Harsha-Buddhika-Official@users.noreply.github.com>
2026-02-22 11:30:43 +0530

    Remove unused import and redundant setForeground
    Remove unused import of java.util.Locale.Category and delete redundant setForeground(Color.BLACK) calls in ui/ManagerDashboard.java since styleButton already applies the color. Also add the compiled ManagerDashboard.class build artifact. No functional changes.

--- ui/ManagerDashboard.java
+++ ui/ManagerDashboard.java
@@ -9,7 +9,6 @@ import com.citybookshop.util.Constants;
 import javax.swing.*;
 import java.awt.*;
 import java.util.List;
-import java.util.Locale.Category;
 
 // Inheritance: ManagerDashboard extends CashierDashboard
 // Manager has all cashier capabilities + additional manager-only features
@@ -58,12 +57,10 @@ public class ManagerDashboard extends CashierDashboard {
 
         JButton addBookBtn = new JButton("+ Add New Book");
         styleButton(addBookBtn, new Color(0, 0, 0));
-        addBookBtn.setForeground(Color.BLACK);
         addBookBtn.addActionListener(e -> showAddBookDialog());
 
         JButton addCategoryBtn = new JButton("+ Add Category");
         styleButton(addCategoryBtn, new Color(0, 0, 0));
-        addCategoryBtn.setForeground(Color.BLACK);
         addCategoryBtn.addActionListener(e -> showAddCategoryDialog());
 
         JButton createAccountBtn = new JButton("+ Create Account");
